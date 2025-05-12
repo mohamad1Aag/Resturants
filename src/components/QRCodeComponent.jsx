cfbe7d889e3f010477a55221a7042fc2cfae6aa2
Mohammad <aagmohamad4@gmail.com>
2025-05-12 22:57:01 +0300

    تحديثات

--- src/components/QRCodeComponent.jsx
+++ src/components/QRCodeComponent.jsx
@@ -5,7 +5,7 @@ function QRCodeComponent() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h2>Scan the QR Code</h2>
-      <QRCode value="https://mohamad1aag.github.io/Resturants/" />
+      <QRCode value="https://resturants-fkmb.vercel.app/" />
     </div>
   );
 }
